46329097cfeb438bde312fb52b570bb8977784c3
panospap004 <pappanos004@gmail.com>
2025-04-01 00:27:36 +0300

    n

--- init.el
+++ init.el
@@ -6,27 +6,16 @@
 ;;
 ;; Delete this once you're comfortable with your setup.
 
-;; (load "~/.configs/emacs/lisp/emacs-settings.el")
-
-;; (Any manual appearance tweaks that are not package‐related can be kept here.)
-
-;; (Place any additional manual modeline tweaks here.)
+(require 'configs)
 
 (setq user-emacs-directory "~/.config/MainEmacs/")
 (add-to-list 'load-path (expand-file-name "lisp" user-emacs-directory))
 
 (require 'example)
 (require 'package_configs)  ;; Now loaded here instead of in config.org
-(require 'configs)
 (require 'keymaps)
 (require 'packages)
 ;; The previous (require 'packages) has been replaced by loading package_configs.el below.
-
-;; Load the package installation (packages.el) as before.
-;; (load "~/.configs/emacs/lisp/packages.el")
-
-;; Load all package-specific configuration settings.
-;; (load "~/.configs/emacs/lisp/package_configs.el")
 
 ;; (start/hello)
 
