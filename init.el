0b9a43b64d0039433c939e8e61dd9abf31ef179a
panospap004 <pappanos004@gmail.com>
2025-04-01 00:20:15 +0300

    moved emacs configs and placed them in config instead

--- init.el
+++ init.el
@@ -7,111 +7,35 @@
 ;;
 ;; You can delete this when you're done. It's your config now. :)
 
-;; The default is 800 kilobytes. Measured in bytes.
-(setq gc-cons-threshold (* 50 1000 1000))
-
-(defun start/org-babel-tangle-config ()
-  "Automatically tangle our Emacs.org config file when we save it. Credit to Emacs From Scratch for this one!"
-  (when (string-equal (file-name-directory (buffer-file-name))
-                      (expand-file-name user-emacs-directory))
-    ;; Dynamic scoping to the rescue
-    (let ((org-confirm-babel-evaluate nil))
-      (org-babel-tangle))))
-
-(add-hook 'org-mode-hook (lambda () (add-hook 'after-save-hook #'start/org-babel-tangle-config)))
-
-(use-package emacs
-  :custom
-  (menu-bar-mode nil)         ;; Disable the menu bar
-  (scroll-bar-mode nil)       ;; Disable the scroll bar
-  (tool-bar-mode nil)         ;; Disable the tool bar
-  ;;(inhibit-startup-screen t)  ;; Disable welcome screen
-
-  (delete-selection-mode t)   ;; Select text and delete it by typing.
-  (electric-indent-mode nil)  ;; Turn off the weird indenting that Emacs does by default.
-  (electric-pair-mode t)      ;; Turns on automatic parens pairing
-
-  (blink-cursor-mode nil)     ;; Don't blink cursor
-  (global-auto-revert-mode t) ;; Automatically reload file if it has changed
-
-  ;;(dired-kill-when-opening-new-dired-buffer t) ;; Dired don't create new buffer
-  ;;(recentf-mode t) ;; Enable recent file mode
-
-  ;;(global-visual-line-mode t)           ;; Enable truncated lines
-  (display-line-numbers-type 'relative) ;; Relative line numbers
-  (global-display-line-numbers-mode t)  ;; Display line numbers
-
-  (mouse-wheel-progressive-speed nil) ;; Disable progressive speed when scrolling
-  (scroll-conservatively 10) ;; Smooth scrolling
-  ;;(scroll-margin 8)
-
-  (tab-width 4)
-
-  (make-backup-files nil) ;; Stop creating ~ backup files
-  (auto-save-default nil) ;; Stop creating # auto save files
-  :hook
-  (prog-mode . (lambda () (hs-minor-mode t))) ;; Enable folding (hide/show) in programming modes
-  :config
-  ;; Move customization variables to a separate file to keep init.el clean.
-  (setq custom-file (locate-user-emacs-file "custom-vars.el"))
-  (load custom-file 'noerror 'nomessage)
-  :bind
-  ([escape] . keyboard-escape-quit)) ;; Makes Escape quit prompts (Minibuffer Escape)
+;; (load "~/.configs/emacs/lisp/emacs-settings.el")
 
 (use-package gruvbox-theme
   :config
-  (load-theme 'gruvbox-dark-medium t)) ;; We need to add t to trust this package
-
-(add-to-list 'default-frame-alist '(alpha-background . 100)) ;; For all new frames henceforth
-
-(set-face-attribute 'default nil
-                    ;; :font "JetBrains Mono" ;; Set your favorite type of font or download JetBrains Mono
-                    :height 120
-                    :weight 'medium)
-;; This sets the default font on all graphical frames created after restarting Emacs.
-;; Does the same thing as 'set-face-attribute default' above, but emacsclient fonts
-;; are not right unless I also add this method of setting the default font.
-
-;;(add-to-list 'default-frame-alist '(font . "JetBrains Mono")) ;; Set your favorite font
-;; Set the default font for the initial frame
-(add-to-list 'initial-frame-alist '(font . "MonaspiceRn Nerd Font-16.5"))
-
-;; Set the default font for new frames
-(add-to-list 'default-frame-alist '(font . "MonaspiceRn Nerd Font-16.5"))
-
-(setq-default line-spacing 0.12)
-
-(use-package emacs
-  :bind
-  ("C-+" . text-scale-increase)
-  ("C--" . text-scale-decrease)
-  ("<C-wheel-up>" . text-scale-increase)
-  ("<C-wheel-down>" . text-scale-decrease))
+  (load-theme 'gruvbox-dark-medium t)) ;; Trust the theme by passing t
 
 (use-package doom-modeline
   :init (doom-modeline-mode 1)
   :custom
-  (doom-modeline-height 25)     ;; Sets modeline height
-  (doom-modeline-bar-width 5)   ;; Sets right bar width
-  (doom-modeline-persp-name t)  ;; Adds perspective name to modeline
-  (doom-modeline-persp-icon t)) ;; Adds folder icon next to persp name
+  (doom-modeline-height 25)     ;; Modeline height
+  (doom-modeline-bar-width 5)   ;; Right bar width
+  (doom-modeline-persp-name t)  ;; Show perspective name
+  (doom-modeline-persp-icon t)) ;; Show folder icon next to perspective name
 
 (use-package projectile
   :init
   (projectile-mode)
   :custom
-  (projectile-run-use-comint-mode t) ;; Interactive run dialog when running projects inside Emacs (like giving input)
-  (projectile-switch-project-action #'projectile-dired) ;; Open dired when switching to a project
-  (projectile-project-search-path '("~/projects/" "~/work/" ("~/github" . 1)))) ;; . 1 means only search the first subdirectory level for projects
-;; Use Bookmarks for smaller, not standard projects
+  (projectile-run-use-comint-mode t)
+  (projectile-switch-project-action #'projectile-dired)
+  (projectile-project-search-path '("~/projects/" "~/work/" ("~/github" . 1))))
 
 (use-package eglot
-  :ensure nil ;; Don't install eglot because it's now built-in
+  :ensure nil
   :hook ((c-mode c++-mode lua-mode) . eglot-ensure)
   :custom
-  (eglot-events-buffer-size 0) ;; No event buffers (Lsp server logs)
-  (eglot-autoshutdown t)         ;; Shutdown unused servers.
-  (eglot-report-progress nil))   ;; Disable LSP server logs
+  (eglot-events-buffer-size 0)
+  (eglot-autoshutdown t)
+  (eglot-report-progress nil))
 
 (use-package yasnippet-snippets
   :hook (prog-mode . yas-minor-mode))
@@ -127,10 +51,10 @@
 (require 'configs)
 (require 'keymaps)
 (require 'packages)
-;; Removed (require 'packages) in favor of loading packages.el below.
+;; (require 'packages) has been removed in favor of loading packages.el below.
 
 ;; Load the package installation setup from packages.el
-;; (load "~/.configs/emacs/lisp/packages")
+;; (load "~/.configs/emacs/lisp/packages.el")
 
 ;; (start/hello)
 
@@ -240,6 +164,3 @@
   (which-key-idle-delay 0.8)
   (which-key-max-description-length 25)
   (which-key-allow-imprecise-window-fit nil))
-
-(setq gc-cons-threshold (* 2 1000 1000))
-(setq read-process-output-max (* 1024 1024)) ;; 1MB
